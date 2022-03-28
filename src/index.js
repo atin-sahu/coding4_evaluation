bf18411a8bb9305389cb6890493f3c851c410849
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-03-28 11:43:32 +0530

    done

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const express = require("express");
 const app = express();
 
 const {register, login, generatetoken} = require("./controller/auth_controller");
+const todoController = require("./controller/todo_controller");
 app.use(express.json());
 
 app.post("/register", register)
 app.post("/login", login)
 
+app.use("/todo", todoController);
 module.exports = app;
